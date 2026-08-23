69efef4b788b1e477039fe691fd7491a739b7c0d
Dhairya-Saxena <dhairyasaxena993@gmail.com>
2026-08-23 19:17:11 +0530

    Update program15.c

--- program15.c
+++ program15.c
@@ -6,7 +6,7 @@ printf("Enter a character");
 scanf("%c", &a);
 int k=(int)a;
 if (k>=65 && k<=90){
-printf("Upperccase \n");
+printf("Uppercase \n");
 } else if (k>=97 && k<=127) {
 printf("Lowercase \n");
 } else if (k>=48 && k<=57) {
