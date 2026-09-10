468528d8ef35efcd82fa8f7c0ad203480c3c0584
Dhairya-Saxena <dhairyasaxena993@gmail.com>
2026-09-10 21:13:37 +0530

    Add files via upload

---
+++ program45.c
@@ -0,0 +1,13 @@
+// Q45: Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+#include <stdio.h>
+int main(){
+int n;
+printf("Enter the number of terms:");
+scanf("%d", &n);
+double sum = 0.0;
+for (int i = 1; i <= n; i++) {
+sum+=(double)(2*i)/(4*i-1);
+}
+printf("Approximate sum: %.2f\n", sum);
+return 0;
+}
