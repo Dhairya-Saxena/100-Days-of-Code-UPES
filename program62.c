3b6819f185cd4e1bca83d218d820471a8e1a80bd
Dhairya-Saxena <dhairyasaxena993@gmail.com>
2026-09-17 21:56:53 +0530

    Add files via upload

---
+++ program62.c
@@ -0,0 +1,26 @@
+// Q62: Reverse an array without taking extra space.
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
